0cf10faa1bbc6406ef61e895c33d0bfc9f3333b5
AnastasiaTsvetkova <86355641+AnastasiaTsvetkova@users.noreply.github.com>
2021-06-28 12:30:39 +0300

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,26 @@
+﻿// lab1
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    const int K = 10;
+    int A[K];
+    cout << "Введите элементы массива:" << endl;
+    for (int i = 0; i < K; i++)
+    {
+        cin >> A[i];                                
+    }
+    for (int i = 0; i < K; i++)
+    {
+        if (A[i] % 2 == 0)  
+            A[i] = 0;
+    }
+    cout << "Mассив:" << endl;
+    for (int i = 0; i < K; i++)
+        cout << setw(2) << A[i] << " ";                      
+    system("pause");
+    return 0;
+}
